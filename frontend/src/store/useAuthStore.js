8902c0ddc8655d4f9c939c8e4ed19796897a09eb
Pankhuri Singh <spankhuri1808@gmail.com>
2025-06-02 22:13:52 +0530

    1

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import {axiosInstance} from "../lib/axios.js"
 import toast from "react-hot-toast";
 import { LogOut } from "lucide-react";
 
-const BASE_URL=import.meta.env.MODE==="development"?"http://localhost:5001/api":"/api";
+const BASE_URL=import.meta.env.MODE==="development"?"http://localhost:5001/api":"/";
 import {io} from "socket.io-client"
 
 export const useAuthStore=create((set,get)=>({
