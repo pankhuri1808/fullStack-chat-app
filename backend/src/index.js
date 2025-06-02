b14f48e39c78a1f8712251dfda4cbe60311ff5cb
Pankhuri Singh <spankhuri1808@gmail.com>
2025-06-02 22:36:26 +0530

    original

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,6 @@ import cookieParser from "cookie-parser"
 import cors from "cors";
 
 import path from "path"
-import { fileURLToPath } from 'url';
 
 import {connectDB} from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js"
@@ -15,8 +14,7 @@ import {app,server} from "./lib/socket.js";
 dotenv.config()
 
 const PORT=process.env.PORT;
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname=path.resolve();
 
 app.use(express.json({ limit: "10mb" })); // Increase limit
 app.use(express.urlencoded({ limit: "10mb", extended: true })); 
@@ -30,10 +28,10 @@ app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
   
     app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
+      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
     });
   }
 
